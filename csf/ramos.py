fa06df85aae93200a47c47e50d1b9f874a6bbb88
tiagolb <tiagoluisbrito@gmail.com>
2015-11-02 21:34:16 +0000

    [FIX] Fixed project structure

--- csf/ramos.py
+++ csf/ramos.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import argparse
-from twitter import twitter
-from cli_parser import *
+from platforms import *
+from interfaces import *
 
 def main():
 
@@ -20,7 +20,8 @@ def main():
     # * CHOICES[3] = 'pidgin'
 
     if CHOICES[0] in targets:
-        twitter(open(filename, "r"))
+        for p in twitter(open(filename, "r")):
+            print p
     # if CHOICES[1] in targets:
     #     facebook(open(filename, "r"))
     # if CHOICES[2] in targets:
