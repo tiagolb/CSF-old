a5805be5283b077a453e43e28fdb7dd736be7365
Diogo Barradas <diogo.barradas@outlook.com>
2015-11-04 19:00:55 +0000

    Added Parsing module for FB recent threads

---
+++ csf/platforms/facebookThreads.py
@@ -0,0 +1,56 @@
+import sys
+import re
+
+class FacebookThreadsParser:
+    def get_facebook_threads_set(self, input_file):
+        strings_joined = '\n'.join(input_file.readlines())
+
+        processed_input = re.sub(r'\\(.)', r'\1', strings_joined)
+
+        #Input split into message blocks tuples
+        begin = r'{\"thread_id\":(.+?)titan_originated_thread_id'
+
+        message_block_regex = re.compile(
+            '.*?'.join([begin]),
+            re.VERBOSE | re.DOTALL)
+        
+        messageTuples = message_block_regex.findall(processed_input)
+        """
+        for i in messageTuples:
+            print i+"\n"
+        """
+        # Regex for intel extraction from each tuple
+        handler = r'\"snippet_sender\":\"fbid:(\d+)'
+        date = r'\"timestamp\":(\d+)'
+        content = r'\"snippet\":\"(.+?)\",\"snippet_has_attachment'
+        recipient = r'\"thread_fbid\":\"(\d+)'
+        participants = r'\"participants\":\[(.+?)\],'
+        former_participants = r'\"former_participants\":\[(.*?)\],'
+        message_count = r'\"message_count\":(\d+)'
+
+        thread_regex = re.compile(
+            '.*?'.join([recipient, participants, former_participants, content, handler, message_count, date]),
+            re.VERBOSE | re.DOTALL)
+
+        results = []
+        
+        for t in messageTuples:
+            Threads = thread_regex.findall(t)
+            results.append(Threads[0])
+
+        return set(results)
+        
+    def get_facebook_threads_timeline(self, facebook_list):
+        return sorted(facebook_threads_list, key=lambda t_list: t_list[1])
+
+
+    def get_timeline(self, input_file):
+        facebook_threads_list     = self.get_facebook_threads_set(input_file)
+        facebook_threads_timeline = self.get_facebook_threads_timeline(facebook_threads_list)
+        return facebook_threads_timeline
+
+
+if __name__ == '__main__':
+    facebook_threads = FacebookThreadsParser()
+    for entry in facebook_threads.get_facebook_threads_set(open(sys.argv[1], "r")):
+        print entry
