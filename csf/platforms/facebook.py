7e482e263a557846debafe0baa9c8b1203d31f5c
David Duarte <david.duarte@tecnico.ulisboa.pt>
2015-11-09 18:53:48 +0000

    Remove unnecessary join in facebook.py and facebookThreads.py

--- csf/platforms/facebook.py
+++ csf/platforms/facebook.py
@@ -1,20 +1,19 @@
 import sys
 import re
 
+
 class FacebookParser:
     def get_facebook_set(self, input_file):
         strings_joined = '\n'.join(input_file.readlines())
 
         processed_input = re.sub(r'\\(.)', r'\1', strings_joined)
 
-        #Input split into message blocks tuples
+        # Input split into message blocks tuples
         begin = r'\[thread_id\]&message_batch\[0\](.+?)&client=mercury'
 
-        message_block_regex = re.compile(
-            '.*?'.join([begin]),
-            re.VERBOSE | re.DOTALL)
+        message_block_regex = re.compile(begin, re.VERBOSE | re.DOTALL)
 
-        messageTuples = message_block_regex.findall(processed_input)
+        message_tuples = message_block_regex.findall(processed_input)
 
         # Regex for intel extraction from each tuple
         handler = r'\[author\]=fbid%3A(\d+)'
@@ -28,26 +27,25 @@ class FacebookParser:
             re.VERBOSE | re.DOTALL)
 
         group_talk_regex = re.compile(
-        '.*?'.join([handler, date, content, group_recipient]),
-        re.VERBOSE | re.DOTALL)
+            '.*?'.join([handler, date, content, group_recipient]),
+            re.VERBOSE | re.DOTALL)
 
         results = []
 
-        for t in messageTuples:
-            singleTalk = single_talk_regex.findall(t)
-            if(len(singleTalk)>0):
-                results.append(singleTalk[0])
+        for t in message_tuples:
+            single_talk = single_talk_regex.findall(t)
+            if len(single_talk) > 0:
+                results.append(single_talk[0])
                 continue
             else:
-                groupTalk = group_talk_regex.findall(t)
-                if(len(groupTalk)>0):
-                    results.append(groupTalk[0])
+                group_talk = group_talk_regex.findall(t)
+                if len(group_talk) > 0:
+                    results.append(group_talk[0])
 
         return set(results)
 
     def get_facebook_timeline(self, facebook_list):
         return sorted(facebook_list, key=lambda t_list: t_list[1])
-
 
     def get_timeline(self, input_file):
         facebook_list     = self.get_facebook_set(input_file)
@@ -57,5 +55,5 @@ class FacebookParser:
 
 if __name__ == '__main__':
     facebook = FacebookParser()
-    for entry in facebook.get_facebook_list(open(sys.argv[1], "r")):
+    for entry in facebook.get_facebook_set(open(sys.argv[1], "r")):
         print entry
