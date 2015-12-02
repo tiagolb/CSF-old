2c856585440bcdc55baa95fdd4856c066f4245b3
Tiago Brito <tiagolb@users.noreply.github.com>
2015-12-02 09:35:02 +0000

    Update cli_parser.py

--- csf/interfaces/cli_parser.py
+++ csf/interfaces/cli_parser.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python
+'''
+The MIT License (MIT)
+
+Copyright (c) 2015 Tiago Brito
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+
+__author__ = "Tiago Brito, Diogo Barradas, David Duarte"
+__copyright__ = "Copyright 2015, Tiago Brito"
+__credits__ = ["Tiago Brito", "Diogo Barradas", "David Duarte"]
+__license__ = "MIT"
+__version__ = "1.0"
+__maintainer__ = "Tiago Brito"
+__email__ = "tiago.de.oliveira.brito@tecnico.ulisboa.pt"
+__status__ = "Production"
+
 import argparse
 import sys
 import os
@@ -28,27 +58,34 @@ def do_extract(args, targets):
             print "\t[+] %s" % target
 
     return_targets = {}
+    # for all targets installed
     for key, value in targets.iteritems():
+        # if that target was selected insert it to the list
         if key in args.targets:
             return_targets[key] = value
 
-    args.targets       = return_targets
-
-
+    args.targets = return_targets
     return args
 
 def do_create(args, targets):
     EXTERNAL = 'external'
+    
+    # create external/ folder if there is not any
     if not os.path.exists(EXTERNAL):
         os.makedirs(EXTERNAL)
 
+    # save the local path to retrun to later
     savedPath = os.getcwd()
+    # change dir to external
     os.chdir(EXTERNAL)
 
+    # download the example module from these URLs
     url_init   = 'http://web.ist.utl.pt/ist172647/ramas/external/__init__.py'
     url_module = 'http://web.ist.utl.pt/ist172647/ramas/external/newtwitter.py'
     wget.download(url_init)
     wget.download(url_module)
+    
+    # return to previous dir
     os.chdir(savedPath)
     print '\n'
 
